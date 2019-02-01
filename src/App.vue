96242dab1462bcdb42f81d88d707dcd6c9ec4ecc
Pevnev,  Maksim <m.pevnev@itransition.com>
2019-02-01 19:13:11 +0300

    dev: add Todo functionality

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
     <div id="app">
+        <Header />
+        <AddTodo v-on:add-todo="addTodo"/>
         <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
     </div>
 </template>
 
 <script>
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
     name: 'app',
     components: {
+        Header,
         Todos,
+        AddTodo,
     },
     data() {
         return {
@@ -37,19 +43,35 @@ export default {
         deleteTodo (id) {
             this.todos = this.todos.filter(todo => todo.id !== id);
         },
+        addTodo(newTodo) {
+            this.todos = [...this.todos, newTodo];
+        }
     }
 }
 </script>
 
 <style>
-  * {
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-  }
+    * {
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+    }
 
-  body {
-    font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+    body {
+        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+        line-height: 1.4;
+    }
+
+    .btn {
+        display: inline-block;
+        border: none;
+        background: #555;
+        color: #fff;
+        padding: 7px 20px;
+        cursor: pointer;
+    }
+
+    .btn:hover {
+        background: #666;
+    }
 </style>
