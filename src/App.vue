7428fd6d35150d4b31f08212e5af9a7894bfe7f0
Pevnev,  Maksim <m.pevnev@itransition.com>
2019-02-01 18:37:29 +0300

    dev: make todos with deleting

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        Todos,
+    },
+    data() {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    title: "TODO One",
+                    completed: false,
+                },
+                {
+                    id: 2,
+                    title: "TODO Two",
+                    completed: true,
+                },
+                {
+                    id: 3,
+                    title: "TODO Three",
+                    completed: false,
+                },
+            ]
+        }
+    },
+    methods: {
+        deleteTodo (id) {
+            this.todos = this.todos.filter(todo => todo.id !== id);
+        },
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
